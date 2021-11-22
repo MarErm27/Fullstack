de30e9252213fc879d0fe7b5dfa33f2e0942c9e4
MarErm27 <v27i72@gmail.com>
2021-11-22 13:50:40 +0300

    1.1: course information, step1

--- part1/src/App.js
+++ part1/src/App.js
@@ -30,16 +30,10 @@ const Total = (props) => {
 
 const App = () => {
     const course = 'Half Stack application development'
-    // const part1 = 'Fundamentals of React'
     const part1 = {
         name: 'Fundamentals of React',
         exercises: 10
     }
-    // const exercises1 = 10
-    // const part2 = 'Using props to pass data'
-    // const exercises2 = 7
-    // const part3 = 'State of a component'
-    // const exercises3 = 14
     const part2 = {
         name: 'Using props to pass data',
         exercises: 7
