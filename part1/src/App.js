6c53cef211f1291895ddd45a4b84d66e5ec85f75
MarErm27 <v27i72@gmail.com>
2021-11-23 11:29:25 +0300

    1.11*: unicafe step6

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 
 const StatisticLine = ({text, value}) => {
-    return <div>{text} {value}</div>
+    return <tr><td>{text}</td> <td>{value}</td></tr>
 }
 
 const Statistics = ({good, neutral, bad}) => {
@@ -11,14 +11,16 @@ const Statistics = ({good, neutral, bad}) => {
     }
     const average = (good * 100 / all - bad * 100 / all) * 0.01
     const positive = (good * 100 / all) + " %"
-    return <div>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={all} />
-        <StatisticLine text="average" value={average} />
-        <StatisticLine text="positive" value={positive} />
-    </div>
+    return <table>
+        <tbody>
+        <StatisticLine text="good" value={good}/>
+        <StatisticLine text="neutral" value={neutral}/>
+        <StatisticLine text="bad" value={bad}/>
+        <StatisticLine text="all" value={all}/>
+        <StatisticLine text="average" value={average}/>
+        <StatisticLine text="positive" value={positive}/>
+        </tbody>
+    </table>
 }
 
 const Button = (props) => (
