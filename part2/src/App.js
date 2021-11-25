2014f9cf8dc680e4ed98db78ce71343dbf14b7ed
MarErm27 <v27i72@gmail.com>
2021-11-25 17:02:31 +0300

    2.7: The Phonebook Step2

--- part2/src/App.js
+++ part2/src/App.js
@@ -10,6 +10,11 @@ const App = () => {
 
     const addPerson = (event) => {
         event.preventDefault()
+        if (persons.map(person => person.name).indexOf(newName) !== -1) {
+            alert(`${newName} is already added to phonebook`)
+            return
+        }
+
         const personObject = {
             name: newName
         }
