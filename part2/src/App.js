e0d1b4b7d749d0816c601245273c40a2a8347292
MarErm27 <v27i72@gmail.com>
2021-11-26 10:12:28 +0300

    2.9*: The Phonebook Step4

--- part2/src/App.js
+++ part2/src/App.js
@@ -4,13 +4,14 @@ import Person from "./components/Person";
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {
-            name: 'Arto Hellas',
-            phone: '040-1234567'
-        }
+        {name: 'Arto Hellas', number: '040-123456', id: 1},
+        {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
+        {name: 'Dan Abramov', number: '12-43-234345', id: 3},
+        {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
     ])
     const [newName, setNewName] = useState('')
     const [newPhone, setNewPhone] = useState('')
+    const [filterString, setFilterString] = useState('')
 
     const addPerson = (event) => {
         event.preventDefault()
@@ -28,6 +29,10 @@ const App = () => {
         setNewPhone('')
     }
 
+    const personsToShow = filterString.length === 0
+        ? persons
+        : persons.filter(person => person.name.toLowerCase().indexOf(filterString.toLowerCase()) !== -1)
+
     const handleNameChange = (event) => {
         setNewName(event.target.value)
     }
@@ -35,9 +40,19 @@ const App = () => {
         setNewPhone(event.target.value)
     }
 
+    const handleFilterStringChange = (event) => {
+        setFilterString(event.target.value)
+    }
+
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>filter shown with <input
+                value={filterString}
+                onChange={handleFilterStringChange}
+            />
+            </div>
+            <h2>add a new</h2>
             <form onSubmit={addPerson}>
                 <div> name: <input
                     value={newName}
@@ -55,7 +70,7 @@ const App = () => {
             </form>
             <h2>Numbers</h2>
             <ul>
-                {persons.map(person =>
+                {personsToShow.map(person =>
                     <Person key={person.name} name={person.name} phone={person.phone}/>
                 )}
             </ul>
